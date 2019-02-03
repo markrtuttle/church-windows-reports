a2877c9c6c6ece66a18b5b64db0470d0d1b0719d
Mark R. Tuttle <mrtuttle@alum.mit.edu>
2019-02-03 07:47:38 -0500

    Tree object used to create ministry reports.

--- report_ministry.py
+++ report_ministry.py
@@ -3,9 +3,17 @@
 import datet
 import report_style
 
+import summary
+import summary_budget
+
+import detail
+import entriest
+
+import treet
+
 ################################################################
 
-def ministry_reports(chart, journal, budget, balance, ministry, period_name,
+def ministry_reports(chart, tree, journal, budget, balance, ministry, period_name,
                      date_start, date_end, posted_start, posted_end,
                      width=80):
 
@@ -25,7 +33,7 @@ def ministry_reports(chart, journal, budget, balance, ministry, period_name,
                                               datet.to_string(date_end))
         print "Deacons: {}".format(', '.join(ministry.deacons(name)))
         print
-        ministry_summary(name, period_name, balance, chart, budget, ministry,
+        ministry_summary(name, period_name, tree, balance, chart, budget, ministry,
                          width=width)
         print
         ministry_detail(name, journal, chart, ministry,
@@ -34,7 +42,7 @@ def ministry_reports(chart, journal, budget, balance, ministry, period_name,
 
 ################################################################
 
-def ministry_summary(name, period_name, balance, chart, budget, ministry,
+def ministry_summary(name, period_name, tree, balance, chart, budget, ministry,
                      width=80,
                      name_w=21,
                      amount_w=8,
@@ -44,18 +52,18 @@ def ministry_summary(name, period_name, balance, chart, budget, ministry,
     # pylint: disable=too-many-arguments
 
     numbers = ministry.accounts(name)
-    report_style.display_budgeted_accounts(
-        "General Fund", period_name, numbers, chart, balance, budget,
-        width, name_w, amount_w, balance_w, name_max)
+    trees = [tree.tree(num) for num in numbers]
+    if trees:
+        summary_budget.tree_summaries(trees, chart, balance, budget, report_name="General Fund", activity_name=period_name)
 
     if numbers:
         print
 
     numbers = ministry.funds(name)
-    report_style.display_accounts(
-        "Other Funds", period_name, numbers, chart, balance,
-        width, name_w, amount_w, balance_w, name_max)
-
+    trees = [tree.tree(num) for num in numbers]
+    trees = [treet.remove_income_expense_from_tree(tree) for tree in trees]
+    if trees:
+        summary.tree_summaries(trees, chart, balance, report_name="Other Funds", activity_name=period_name, balance_name=period_name)
 ################################################################
 
 def ministry_detail(name, journal, chart, ministry,
@@ -82,13 +90,18 @@ def ministry_detail(name, journal, chart, ministry,
                  and
                  entry.date_is(None, posted_end)))]
 
-    report_style.display_entries(entries, chart,
-                                 group_by_month=False,
-                                 sort_keys=['name', 'reverse_amount', 'date'],
-                                 width=width,
-                                 comment_w=comment_w,
-                                 name_w=name_w,
-                                 amount_w=amount_w,
-                                 name_max=name_max)
+    entries = journal.entries()
+    entries = entriest.select_by_number(entries, numbers)
+    entries = entriest.select_by_date(entries, date_start, date_end,
+                                      posted_start, posted_end)
+    groups = entriest.group_by_month(entries, reverse=True)
+    groups = [entriest.sort_by_amount(group, reverse=True) for group in groups]
+    groups = [entriest.sort_by_name(group, chart) for group in groups]
+    
+    for group in groups:
+        print
+        detail.detail(group, credit=True, width=width,
+                      comment_w=comment_w, name_w=name_w, amount_w=amount_w,
+                      name_max=name_max)
 
 ################################################################
